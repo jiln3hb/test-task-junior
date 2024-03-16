cec6ba6eeab7015035f51f2bd0476802f501d3b4
pollitod <pollitod@student.21-school.ru>
2024-03-16 17:08:18 +0700

    init commit

---
+++ src/main/java/com/test/testtaskjunior/TestTaskJuniorApplication.java
@@ -0,0 +1,13 @@
+package com.test.testtaskjunior;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTaskJuniorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTaskJuniorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/testtaskjunior/TestTaskJuniorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.testtaskjunior;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskJuniorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
